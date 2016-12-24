420a02aaaaf6ff0091597ae1a7ced29b6059d226
zaragozamartin91 <zaragozamartin91@gmail.com>
2016-12-24 00:45:33 -0300

    Se agrego la query para agregar un articulo

--- nhozdata/index.js
+++ nhozdata/index.js
@@ -8,31 +8,51 @@ var pool = mysql.createPool({
     database: config.database
 });
 
-
-module.exports.addProvider = function (id, name) {
+function poolAction(callback) {
     pool.getConnection(function (err, connection) {
         if (err) {
             console.error("OCURRIO UN ERROR AL OBTENER UNA CONEXION CON LA BBDD: " + err);
             return;
         }
+        callback(connection);
+        connection.release();
+    });
+}
 
+module.exports.addProvider = function (id, name, errCallback, succCallback) {
+    poolAction(function (connection) {
         if (id) {
             name = name || `Proveedor ${id}`;
+            errCallback = errCallback || function () { };
+            succCallback = succCallback || function () { };
 
             // Use the connection
             connection.query(`INSERT INTO ${config.providerTableName} VALUES ('${id}','${name}')`,
                 function (err, rows) {
                     if (err) {
                         console.error('OCURRIO UN ERROR AL AGREGAR UN PROVEEDOR: ' + err);
+                        errCallback();
                     } else {
                         console.log(`PROVEEDOR ${id}::${name} AGREGADO EXITOSAMENTE`);
+                        succCallback();
                     }
                 });
         } else {
             console.error('NO SE INGRESO UN ID DE PROVEEDOR');
         }
+    });
+};
 
-        connection.release();
+module.exports.addArticle = function (providerId, id, description, price) {
+    description = description || `Articulo ${id} de proveedor ${providerId}`;
+
+    poolAction(function (connection) {
+        connection.query(`INSERT INTO ${config.articleTableName} VALUES ("${providerId}","${id}","${description}","${price}")`,
+            function (err) {
+                if (err) {
+                    console.error(`OCURRIO UN ERROR AL INGRESAR EL ARTICULO ${id} DEL PROVEEDOR ${providerId}: ${err}`);
+                }
+            });
     });
 };
 
