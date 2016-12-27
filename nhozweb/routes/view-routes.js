2e902a64f08fdcdc74e8edf4f3f3af15651214ea
Martin <zaragozamartin91@gmail.com>
2016-12-27 15:06:01 -0300

    Se hizo un ejemplo de pasaje de mensaje de error.

--- nhozweb/routes/view-routes.js
+++ nhozweb/routes/view-routes.js
@@ -9,20 +9,23 @@ router.get('/', function (req, res, next) {
 
 router.get('/providers', function (req, res, next) {
   var provId = req.query.id;
+  var error = req.query.err;
 
   data.getProviderLike({ id: provId }, function (err, rows) {
     res.render('providers', {
       title: 'Proveedores',
       providers: rows,
-      err: err
+      err: error
     });
   });
 });
 
+/* SIGUIENDO ESQUEMA POST/REDIRECT/GET https://es.wikipedia.org/wiki/Post/Redirect/Get  */
 router.post('/providers/add', function (req, res, next) {
   console.log(req.body);
-  req.err = "error!";
-  res.redirect('/providers');
+  /* Necesario para enviar un error mediante la url. */
+  var err = encodeURIComponent("Error al agregar proveedor");
+  res.redirect(`/providers?err=${err}`);
 });
 
 
