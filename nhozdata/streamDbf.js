1695f95312fa7a7579c22e3e88fc092a4b7c5423
zaragozamartin91 <zaragozamartin91@gmail.com>
2016-12-27 22:30:32 -0300

    Se comenzo a trabajar en el script para traspasar todos los datos del dbf a mysql.

--- nhozdata/streamDbf.js
+++ nhozdata/streamDbf.js
@@ -1,12 +1,39 @@
 /* Este es el lector de DBF que mejor funciona. */
 var DBF = require('stream-dbf');
-var parser = new DBF('./LISTAPRE.DBF');
+var parser = new DBF(__dirname + '/LISTAPRE.DBF');
+var mysql = require('mysql');
+var config = require('./config');
+
+var db = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
 
 var stream = parser.stream;
+
+
 stream.on('readable', function () {
     var record = stream.read();
-    console.log(record);
+    if (record) {
+
+        db.query(
+            `INSERT INTO ${config.providerTableName} VALUES ('${record.CODIGOPROV}','Proveedor ${record.CODIGOPROV}')`,
+            function (err) {
+                if (err) {
+                    console.error(`ERROR AL INSERTAR PROVEEDOR ${record.CODIGOPROV}: ${err}`);
+                } else {
+                    console.log(`PROVEEDOR ${record.CODIGOPROV} INSERTADO`);
+                }
+            });
+    };
 });
+
 stream.on('end', function () {
-    console.log('finished');
+    console.log('FINISHED READING');
+    setTimeout(function () {
+        console.log("CERRANDO CONEXION CON BBDD");
+        db.end();
+    }, 20 * 1000);
 });
