29aa18bfaf5209d94bfe28df6f01c2d967ec70f7
zaragozamartin91 <zaragozamartin91@gmail.com>
2017-06-10 16:02:40 -0300

    Se termino la logica de agregado de proveedor y actualizacion de la tabal de proveedores

--- nhozweb/components/providers/ProvidersApp.js
+++ nhozweb/components/providers/ProvidersApp.js
@@ -29,28 +29,50 @@ var ProvidersApp = React.createClass({
     onAddProviderClick: function () {
         console.log(`onAddProviderClick: ${this.state.providerId} ${this.state.providerName}`);
 
-        /* ENVIO UN POST PARA AGREGAR UN PROVEEDOR.
-        USO axios COMO BIBLIOTECA PAR HACER LLAMADAS AJAX */
-        axios.post('/api/providers/add', {
+        let newProvider = {
             id: this.state.providerId,
             name: this.state.providerName
-        }).then(response => {
-            /** SI TODO SALIO BIEN, CAMBIO EL ESTADO DEL COMPONENTE ACTUALIZANDO EL MENSAJE DE EXITO O DE ERROR*/
-            console.log(response);
-            if (response.data.ok) {
-                this.setState({ succMsg: response.data.ok });
-            } else if (response.data.err) {
-                this.setState({ errMsg: response.data.err });
-            }
-        }).catch(error => {
-            /** SI SALIO MAL, CAMBIO EL ESTADO DEL COMPONENTE ACTUALIZANDO EL MENSAJE DE FALLA */
-            console.error(error);
-            this.setState({ errMsg: error });
-        });
+        };
+
+        if (newProvider.id && newProvider.name) {
+            /* ENVIO UN POST PARA AGREGAR UN PROVEEDOR.
+            USO axios COMO BIBLIOTECA PAR HACER LLAMADAS AJAX */
+            axios.post('/api/providers/add', newProvider)
+                .then(response => {
+                    /** SI TODO SALIO BIEN, CAMBIO EL ESTADO DEL COMPONENTE ACTUALIZANDO EL MENSAJE DE EXITO O DE ERROR*/
+                    console.log(response);
+                    if (response.data.ok) {
+                        let providers = this.state.providers;
+                        providers.push(newProvider);
+                        this.setState({
+                            succMsg: response.data.ok,
+                            providers: providers,
+                            errMsg: ""
+                        });
+                    } else if (response.data.err) {
+                        this.setState({ 
+                            errMsg: response.data.err,
+                            succMsg: ""
+                        });
+                    }
+                }).catch(error => {
+                    /** SI SALIO MAL, CAMBIO EL ESTADO DEL COMPONENTE ACTUALIZANDO EL MENSAJE DE FALLA */
+                    console.error(error);
+                    this.setState({ 
+                        errMsg: error,
+                        succMsg: ""
+                    });
+                });
+        } else {
+            this.setState({
+                errMsg: "Ingresar todos los datos del proveedor",
+                succMsg: ""
+            });
+        }
     },
 
-    onRowClick: function(selectedProviders) {
-        this.setState({selectedProviders: selectedProviders});
+    onRowClick: function (selectedProviders) {
+        this.setState({ selectedProviders: selectedProviders });
     },
 
     componentDidMount: function () {
@@ -82,7 +104,7 @@ var ProvidersApp = React.createClass({
 
                 <AddProviderButton onClick={this.onAddProviderClick} />
 
-                <ProvidersTable 
+                <ProvidersTable
                     providers={this.state.providers}
                     onRowClick={this.onRowClick}
                     selectedProviders={this.state.selectedProviders} />
